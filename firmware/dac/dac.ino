7412d5a8949e2aad219030611bf666d7cea8ab63
James Lewis <byerly0503@gmail.com>
2021-03-15 00:15:45 -0500

    added moar blinky
    When a bit is being tested during the binary search, its LEDs now blink.
    
    The screen readout is now 2 lines during the search. First line is current DAC voltage and the second line is the binary code so far with ?, X, 0, and 1 indicators.

--- firmware/dac/dac.ino
+++ firmware/dac/dac.ino
@@ -66,6 +66,14 @@ double measure_dac_value() {
   return analogRead(A5) * (adc_vref/(pow(2,10)));
 }
 
+void print_dac_and_binary(uint16_t color, char *msg) {
+  char str_val[8] = {"0.00v"}; // = "00.00v";
+  double current_dac_voltage = measure_dac_value();
+  Serial.print(F("current_dac_voltage = ")); Serial.print(current_dac_voltage); Serial.println(F(" volts)"));
+  dtostrf(current_dac_voltage, 5, 2, str_val);
+  tft_print_twolines(str_val, color, msg, color); 
+}
+
 void print_dac_value(uint16_t color) {
   char str_val[8] = {"0.00v"}; // = "00.00v";
   double current_dac_voltage = measure_dac_value();
@@ -199,6 +207,44 @@ void setup() {
   dac_cal();
 }
 
+
+
+void led_blink(byte bit, byte test_value) {
+  static unsigned long previous_led_blink=0;
+  static bool led_state = false;
+  unsigned long led_invterval = 50;
+  unsigned long current_millis = millis();
+  if (current_millis - previous_led_blink >= led_invterval) {
+    uint32_t data_stream = convert_to_bit(test_value);
+    if (led_state) {
+      led_state = false;
+      //0x40;
+      bitWrite(data_stream, ((bit*8)-1), 0);  // 4*8, 32-1 = 31
+      bitWrite(data_stream, ((bit*8)-2), 0);  // 4*8, 32-2 = 30
+      bitWrite(data_stream, ((bit*8)-3), 0);
+      bitWrite(data_stream, ((bit*8)-4), 0);
+      bitWrite(data_stream, ((bit*8)-5), 0);
+      bitWrite(data_stream, ((bit*8)-6), 0);
+      bitWrite(data_stream, ((bit*8)-7), 0);
+      bitWrite(data_stream, ((bit*8)-8), 0);  // 48*-8, 32-8 = 24
+    } else {
+      led_state = true;  // 0xBF
+      bitWrite(data_stream, ((bit*8)-1), 1);  // 4*8, 32-1 = 31
+      bitWrite(data_stream, ((bit*8)-2), 0);  // 4*8, 32-2 = 30
+      bitWrite(data_stream, ((bit*8)-3), 1);
+      bitWrite(data_stream, ((bit*8)-4), 1);
+      bitWrite(data_stream, ((bit*8)-5), 1);
+      bitWrite(data_stream, ((bit*8)-6), 1);
+      bitWrite(data_stream, ((bit*8)-7), 1);
+      bitWrite(data_stream, ((bit*8)-8), 1);  // 48*-8, 32-8 = 24
+    }
+   // set_relay(data_stream, true, bit);  // uint32_t data_stream, bool state, int relay_number
+    send_data_to_bits(data_stream);
+    previous_led_blink = current_millis;
+  }
+
+}
+
 void loop() {
   unsigned long current_millis = millis();
 
@@ -234,24 +280,43 @@ void loop() {
 		final_countdown = 0;
 
     // string to display bit sequence on LCD
-    char bit_sequence[dac_width]; 
+    char bit_sequence[dac_width+1]; 
     for (int x=0; x<dac_width; x++)
       bit_sequence[x] = '?';
+    bit_sequence[dac_width+1] = '\0';
 
     // test one bit at a time, going from MSB to LSB
+    const byte remap[] = {3,2,1,0};
 		for (int x=(dac_width-1); x>=0; x--) {
       // test the current  bit
 			bitWrite(final_countdown, x, 1);
 			send_data_to_bits(convert_to_bit(final_countdown));
       delay(10);
-      print_dac_value(LCD_BLUE); // red is actually blue
+      bit_sequence[remap[x]] = 'X';
+      //print_dac_value(LCD_BLUE); // red is actually blue
+      print_dac_and_binary(LCD_BLUE, bit_sequence);
+
       if (stepper) 
-        while(digitalRead(step_button) == NOT_PRESSED);
+        while(digitalRead(step_button) == NOT_PRESSED)
+          led_blink((x+1), final_countdown);
       else 
-        delay(500);
+        //delay(500);
+        for (int ticks=0; ticks<500; ticks++) {
+          led_blink((x+1), final_countdown);
+          delay(1);
+        }
+          
       // capture comparator result, finalizing the current bit
-			bitWrite(final_countdown, x, digitalRead(comparator_result));
-      print_dac_value(LCD_BLUE);
+      if (digitalRead(comparator_result)) {
+		  	bitWrite(final_countdown, x, HIGH);
+        bit_sequence[remap[x]] = '1';
+      } else {
+		  	bitWrite(final_countdown, x, LOW);
+        bit_sequence[remap[x]] = '0';        
+      }
+
+
+      print_dac_and_binary(LCD_BLUE, bit_sequence);  
 		}
 	
 		// send final value	
@@ -262,11 +327,14 @@ void loop() {
     char low_value[8];
     char high_value[8];
 
-    dtostrf((dac_value_step * final_countdown),  5, 2, low_value);
-    dtostrf(((dac_value_step * final_countdown) + dac_value_step), 5, 2, high_value );
+    dtostrf((dac_value_step * final_countdown),  3, 1, low_value);
+    dtostrf(((dac_value_step * final_countdown) + dac_value_step), 3, 1, high_value );
+    char line1[8];
+    sprintf(line1, "%s-%s", low_value, high_value);
 
    // print_dac_value(LCD_RED);
-    tft_print_twolines(low_value, LCD_GREEN, high_value, LCD_RED);
+//    tft_print_twolines(low_value, LCD_GREEN, high_value, LCD_RED);
+    tft_print_twolines(line1, LCD_GREEN, bit_sequence, LCD_BLUE);
 
 		Serial.print(F("Conversion: ")); Serial.print(final_countdown);
 		Serial.print(F(","));
